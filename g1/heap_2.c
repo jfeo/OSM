62a84f7460f04c548c3e837b90cdd705e1bac165
Simon <simwarg@gmail.com>
2015-02-13 23:15:47 +0100

    Commited wrong file contents..

--- g1/heap_2.c
+++ g1/heap_2.c
@@ -1,106 +1,100 @@
+#include "heap.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <assert.h>
-#include "heap.h"
+#include "assert.h"
 
-// Return index of parent of node i
-int heap_parent(int i);
-// Return index of left child of node i
-int heap_left(int i);
-// Return index of right child of node i
-int heap_right(int i);
-// Preserve max heap property of heap
-void heap_max_heapify(heap *h);
-// Rebuild the heap property
-void heap_rebuild_heap(heap *h);
+#define INIT_SIZE 1 // initial capacity of the heap
 
-void heap_initialize(heap *h) {
-  h->size = 0;
-  h->root = NULL;
-  h->alloc_size = 0;
-  h->heapified = 0;
+size_t heap_leftchild(heap* h, size_t i) {
+  assert(h->size > 1 && i < h->size);
+  return i*2+1;
 }
 
-void heap_clear(heap *h) {
+size_t heap_rightchild(heap* h, size_t i) {
+  assert(h->size > 2 && i < h->size);
+  return i*2+2;
+}
+
+size_t heap_parent(heap* h, size_t i) {
+  assert(i < h->size);
+  if(h->size < 2) {
+    return 0;
+  }
+  return (i-1)/2;
+}
+
+void heap_clear(heap* h) {
   free(h->root);
   heap_initialize(h);
 }
 
-size_t heap_size(heap *h) {
-  return h->size;
-}
+void heap_heapify(heap* h, size_t i) {
+  node* parent = &h->root[i];
 
-void* heap_top(heap *h) {
-  if (h->size == 0) return NULL;
-  heap_rebuild_heap(h);
+  size_t largest = i;
 
-  return h->root[0].value;
-}
-
-void heap_insert(heap *h, void *value, int priority) {
-  node n = {value, priority};
-  h->size++;
-  h->alloc_size += sizeof(node);
-  h->root = (node *)realloc(h->root, h->alloc_size);
-  assert(h->root != NULL);
-  h->root[h->size - 1] = n;
-  h->heapified = 0;
-}
-
-void* heap_pop(heap *h) {
-  if (h->size == 0) return NULL;
-  heap_rebuild_heap(h);
-
-  void *ptr = h->root[0].value;
-  h->alloc_size -= sizeof(node);
-  for (int i = 1; i < h->size; i++) {
-    h->root[i - 1] = h->root[i];
-  }
-  h->root = (node *)realloc(h->root, h->alloc_size);
-  assert(h->root != NULL);
-  h->size--;
-  h->heapified = 0;
-  return ptr;
-}
-
-// Helper methods
-
-// Get index of parent
-int heap_parent(int i) {
-  return i / 2;
-}
-
-// Get index of left child
-int heap_left(int i) {
-  return 2 * i;
-}
-
-// Get index of right child
-int heap_right(int i) {
-  return 2 * i + 1;
-}
-
-// Rebuild max heap property
-void heap_max_heapify(heap *h) {
-  for (int i = h->size - 1; i >= 0; --i) {
-    if (heap_left(i) < h->size && h->root[heap_left(i)].priority > h->root[i].priority) {
-      node highest = h->root[heap_left(i)];
-      node lowest = h->root[i];
-      h->root[i] = highest;
-      h->root[heap_left(i)] = lowest;
+  if(h->size > 1) {
+    size_t left = heap_leftchild(h, i);
+    if(h->root[left].priority > parent->priority) {
+      largest = left;
     }
-    if (heap_right(i) < h->size && h->root[heap_right(i)].priority > h->root[i].priority) {
-      node highest = h->root[heap_right(i)];
-      node lowest = h->root[i];
-      h->root[i] = highest;
-      h->root[heap_right(i)] = lowest;
-    } 
+    if(h->size > 2 ) {
+      size_t right = heap_rightchild(h, i);
+      if(h->root[right].priority > h->root[largest].priority) {
+        largest = right;
+      }
+    }
+  }
+  if(largest != i) {
+    // swap
+    node tmp = *parent;
+    *parent = h->root[largest];
+    h->root[largest] = tmp;
+    heap_heapify(h, largest);
   }
 }
 
-void heap_rebuild_heap(heap *h) {
-  if (h->heapified == 0) {
-    heap_max_heapify(h);
-    h->heapified = 1;
+void heap_initialize(heap* h) {
+  size_t init_bytes = INIT_SIZE * sizeof(node);
+  h->size = 0;
+  h->alloc_size = INIT_SIZE;
+  h->root = malloc(init_bytes);
+}
+
+size_t heap_size(heap* h) {
+  return (*h).size;
+}
+
+void heap_insert(heap* h, void* data, int prio) {
+  assert(prio > 0); // we don't allow for negative priorities
+  h->size += 1;
+  if(h->size > h->alloc_size) {
+    // expand and realloc
+    h->alloc_size = h->alloc_size * 2; // double our space
+    int realloc_bytes = h->alloc_size * sizeof(node);
+    h->root = realloc(h->root, realloc_bytes);
   }
+  // insert at end
+  node* end = h->root + h->size - 1;
+  end->value = data;
+  end->priority = prio;
+  // re-sort
+  for (size_t i = (h->size/2); i > 0 ; i--) {
+    heap_heapify(h, i-1);
+  }
+}
+
+void* heap_pop(heap* h) {
+  if(h->size == 0) return NULL;
+  void* value = h->root->value;
+  // pop the top down to bottom (end of array)
+  h->root->priority = 0;
+  heap_heapify(h, 0);
+  h->size--; 
+  return value;
+}
+
+void* heap_top(heap* h) {
+  if(heap_size(h) == 0) return NULL;
+  return h->root->value;
 }
