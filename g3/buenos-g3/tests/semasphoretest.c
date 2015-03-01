56bfbd240a42c49fb6291585ebdf4f42b8ef944e
Simon <simwarg@gmail.com>
2015-03-01 16:28:11 +0100

    User semaphore interface should work

--- g3/buenos-g3/tests/semasphoretest.c
+++ g3/buenos-g3/tests/semasphoretest.c
@@ -1,9 +1,17 @@
 #include "tests/lib.h"
 
 int main(void) {
-	usr_sem_t* s = syscall_sem_open("simon", 0);
-	printf("Semaphore: %p\n", s);
-	s = syscall_sem_open("simon", 0);
-	printf("Semaphore: %p\n", s);
+	usr_sem_t *s = syscall_sem_open("simon", 0);
+	printf("Semaphore should not be NULL: %p\n", s);
+	
+	usr_sem_t *null = syscall_sem_open("simon", 0);
+	printf("Semaphore should be NULL: %p\n", null);
+
+	usr_sem_t *s2 = syscall_sem_open("simon", -1);
+	printf("Semaphore should be %p: %p\n", s, s2);
+
+	usr_sem_t *s3 = syscall_sem_open("diku", 2);
+	printf("Semaphore should not be NULL and not be %p: %p\n", s, s3);
+
 	return 0;
 }
