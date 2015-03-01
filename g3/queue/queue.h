6ce29d544082b1c2edbe042dac65ea4a4573c2fb
jfeo <xws747@alumni.ku.dk>
2015-03-01 17:38:25 +0100

    Moved queue files

--- g3/queue/queue.h
+++ g3/queue/queue.h
@@ -1,9 +1,24 @@
-#include <stdlib.h>
+#include <pthread.h>
 
-typedef struct Queue Queue;
+typedef struct node {
+  void *item;
+  struct node *next;
+} node_t;
 
-Queue* queue_create();
-size_t queue_size(Queue *q);
-void   queue_push(Queue *q, void *x);
-void*  queue_pop(Queue *q);
-int    queue_empty(Queue *q);
+typedef struct queue {
+  pthread_mutex_t head_mtx;
+  pthread_mutex_t tail_mtx;
+  pthread_cond_t cond;
+  pthread_mutexattr_t attr;
+  node_t *head;
+  node_t *tail;
+} queue_t;
+
+/* queue_init initializes a new queue */
+void queue_init(queue_t *q);
+
+/* queue_put adds the item to the tail of the queue */
+void queue_put(queue_t *q, void *item);
+
+/* queue_get removes and returns the head of the queue in item */
+void *queue_get(queue_t *q);
