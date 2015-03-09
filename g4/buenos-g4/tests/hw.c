acb78a3d7c4b6a2113377fe2d165e16bd35a74c1
David Pedersen <david.pdrsn@gmail.com>
2015-03-09 22:09:19 +0100

    Update test program slightly

--- g4/buenos-g4/tests/hw.c
+++ g4/buenos-g4/tests/hw.c
@@ -15,25 +15,3 @@ int main(void) {
 
   return 0;
 }
-
-/* int main2(void) */
-/* { */
-/*   char *name; */
-/*   int count; */
-/*   heap_init(); */
-/*   puts("Hello, World!\n\n"); */
-/*   while (1) { */
-/*     name = (char*)malloc(BUFFER_SIZE); */
-/*     printf("Please enter your name (max %d chars): ", BUFFER_SIZE); */
-/*     count = readline(name, BUFFER_SIZE); */
-/*     if (count == 0) { */
-/*       break; */
-/*     } */
-/*     name[count] = 0; /1* Chomp off newline *1/ */
-/*     printf("And hello to you, %s!\n", name); */
-/*     free(name); */
-/*   } */
-/*   puts("Now I shall exit!\n"); */
-/*   syscall_exit(2); */
-/*   return 0; */
-/* } */
