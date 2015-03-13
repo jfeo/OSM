83482fbe1f8000201d8c2097126256a0cfa88e73
jfeo <xws747@alumni.ku.dk>
2015-03-13 13:41:32 +0100

    fix seek, write

--- g5/buenos-g5/tests/vfs_write.c
+++ g5/buenos-g5/tests/vfs_write.c
@@ -6,27 +6,16 @@ int main(void) {
   const char *name = "[disk]y.txt";
   const char *str = "hello world";
 
-  syscall_create(name, sizeof(str));
-
+  syscall_create(name, strlen(str));
   file = syscall_open(name);
 
-  res = syscall_write(file, str, sizeof(str));
-  if (res != 0) {
-    printf("syscall_write failed!\n\n");
-    printf("res: %d\n\n", res);
-    return 1;
-  }
+  res = syscall_write(file, str, strlen(str));
+  syscall_seek(file, 0);
 
   char buf[128];
-  res = syscall_read(file, &buf, 128);
-  if (res != 0) {
-    printf("syscall_read failed!\n\n");
-    printf("res: %d\n\n", res);
-    return 1;
-  }
-
+  res = syscall_read(file, &buf, strlen(str));
+  
   printf(buf);
-
   syscall_halt();
-  return 0;
+  return res;
 }
